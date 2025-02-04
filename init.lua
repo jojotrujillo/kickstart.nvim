124b53f1ec8eeb6cb6ab9e35f8b108457366dac9
Jordon Trujillo <jordontrujillo@outlook.com>
2025-02-04 10:17:42 -0600

    Add some vim opts and markdown-preview plugin

--- init.lua
+++ init.lua
@@ -156,6 +156,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.spell = true
+vim.opt.spelllang = en_us
+
+vim.opt.textwidth = 110
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
